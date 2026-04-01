6bcfbbb6c4a5f0c47d38bc28faacf0a9328b15dd
Matheus Schuindt <mschuindt@id.uff.br>
2026-03-31 23:20:39 -0300

    adicionada análise descritiva

--- analise.R
+++ analise.R
@@ -5,3 +5,19 @@
 # Leitura dos dados ---------------------------------------------
 
 df <- read.csv("imoveis.csv", sep=";")
+
+# Análise descritiva --------------------------------------------
+
+analise_quanti <- function(x) {
+  round(c(summary(x), "SD"=sd(x)), 2)
+}
+
+for (coluna in names(df)) {
+  cat(coluna, "\n")
+  print(analise_quanti(df[,coluna]))
+  cat("\n")
+}
+
+hist(df$preco)
+
+plot(df$metragem, df$imposto_anual)
