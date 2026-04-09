fa6b8fba989f36c5bbb159acd15f484f30caf4d3
Matheus Schuindt <mschuindt@id.uff.br>
2026-04-08 23:56:40 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,12 @@
+#
+# Análise estatística de dados sobre imóveis
+# (usando branches do git)
+#
+
+# Leitura dos dados --------------------------------------------------
+
+df <- read.csv("imoveis.csv", header=TRUE, sep=";")
+
+# Gráficos -----------------------------------------------------------
+
+# Medidas ------------------------------------------------------------
